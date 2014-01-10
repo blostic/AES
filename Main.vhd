2929e4656f1cb01cee513c4b8910dabc7ef0ff30
blostic <piotr.skibiak@gmail.com>
2014-01-10 20:28:13 +0100

    Add AddRoundKey entity, add mypackage

--- Main.vhd
+++ Main.vhd
@@ -5,6 +5,8 @@ library Inv_S_Box;
 
 USE ieee.std_logic_1164.all;
 USE work.ALL;
+USE work.mypackage.ALL;
+
 USE KeySchedulerFunction.ALL;
 
 ENTITY Main IS
@@ -30,11 +32,36 @@ ARCHITECTURE behavior OF Main IS
        rw, rs, e	 	 : OUT STD_LOGIC; --read/write, setup/data, and enable for lcd
        lcd_data       : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --data signals for lcd
   END COMPONENT;
+
+  COMPONENT Mix_Columns
+  PORT(
+		 mixcolumn_vector_in     :   IN  std_logic_vector(127 downto 0);
+		 mixcolumn_vector_out    :   OUT std_logic_vector(127 downto 0)
+		 );
+	END COMPONENT;
+	
+	signal round_state_to_xor_S  :   STATE_array;
+   signal round_key_S       	  :   STATE_array;
+   signal round_state_xored_S   :   STATE_array;
+		
+	COMPONENT AddRoundKey
+	PORT(
+		 round_state_to_xor   	: 	IN  STATE_array;
+		 round_key 					:  IN  STATE_array;
+		 round_state_xored   	: 	OUT STATE_array
+		 );
+	END COMPONENT;
+	
 	
   signal GeneratedKey: RESULT_BUFFER; -- RESULT_BUFFER is defined in KeySchedulerFunction
- 
   signal initialKey : RoundKeyArrayDoubled; -- RoundKeyArrayDoubled is defined in KeySchedulerFunction  
+  
   constant keyType : std_LOGIC_VECTOR(1 downto 0) := "01"; 
+  
+  signal STATE : STATE_array;
+	
+  SIGNAL shiftrow  : std_logic_vector(127 downto 0);
+  SIGNAL mixcolumn   : std_logic_vector(127 downto 0);
   
   BEGIN
 
@@ -51,19 +78,41 @@ ARCHITECTURE behavior OF Main IS
          lcd_data => lcd_data
      );
 	  
-  initialKey  <= ( X"00", X"01", X"02", X"03", X"04", X"05", X"06", X"07", X"08", X"09", X"0a", X"0b", 
-						 X"0c", X"0d", X"0e", X"0f", X"10", X"11", X"12", X"13", X"14", X"15", X"16", X"17",
-						 X"00", X"00", X"00", X"00", X"00", X"00", X"00", X"00" ); 
+	  mixcolumn_s: Mix_Columns
+		PORT MAP(   
+			mixcolumn_vector_in  => shiftrow,
+			mixcolumn_vector_out => mixcolumn
+		);
+	
+	RoundKeyAdder: AddRoundKey
+		PORT MAP(   
+			round_state_to_xor => round_state_to_xor_S,
+			round_key			 => round_key_S,       	
+			round_state_xored  => round_state_xored_S
+		);
+	
+	
+  initialKey  <= ( X"00", X"01", X"02", X"03",
+						 X"04", X"05", X"06", X"07", 
+						 X"08", X"09", X"0a", X"0b", 
+						 X"0c", X"0d", X"0e", X"0f", 
+						 
+						 X"10", X"11", X"12", X"13", 
+						 X"14", X"15", X"16", X"17",
+						 X"00", X"00", X"00", X"00", 
+						 X"00", X"00", X"00", X"00" ); 
   
-  --state		  <= ( X"00", X"01", X"02", X"03", X"04", X"05", X"06", X"07", X"08", X"09", X"0a", X"0b", X"0c", X"0d", X"0e", X"0f",
-  --						 X"10", X"11", X"12", X"13", X"14", X"15", X"16", X"17" );
+  STATE		  <= ( X"00", X"11", X"22", X"33",
+						 X"44", X"55", X"66", X"77",
+						 X"88", X"99", X"aa", X"bb",
+						 X"cc", X"dd", X"ee", X"ff" );
 						 
   PROCESS(CLK)
     VARIABLE char  :  INTEGER RANGE 0 TO 10 := 0;
   BEGIN
     IF(CLK'EVENT AND CLK = '1') THEN
 		GeneratedKey <= KeyScheduler(initialKey,keyType);
-		
+		STATE(1)(7 downto 0) <= GeneratedKey(1)(7 downto 0) XOR STATE(1)(7 downto 0);
     END IF;
   END PROCESS;
   
