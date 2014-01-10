7d6c3001aef3797e91637ad4a7796fd0e7440d63
blost <piotr.skibiak@gmail.com>
2014-01-10 22:13:59 +0100

    Atache mypackage file

---
+++ mypackage.vhd
@@ -0,0 +1,9 @@
+library IEEE; 
+use IEEE.STD_LOGIC_1164.all; 
+
+package mypackage is 
+	type STATE_array is array (0 to 15) of STD_LOGIC_VECTOR(7 downto 0);
+end mypackage; 
+
+package body mypackage is 
+end mypackage;
